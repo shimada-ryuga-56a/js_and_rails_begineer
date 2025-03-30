670a715b58c8fc01f16f3a16a044e4dc1e388470
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-30 16:49:37 +0900

    add: root page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "top" => "static_pages#top", as: :top
+
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "static_pages#top"
 end
