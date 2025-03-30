f1b4fd90e94dad471c5b811ea7f9bd9c7b0084f0
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-30 21:05:16 +0900

    add: newアクション追加

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -4,6 +4,13 @@ class SetlistsController < ApplicationController
   end
 
   def new
+    @setlist = Setlist.new
+    20.times do
+      @setlist.setlist_items.build
+    end
+    @setlist.setlist_items.each_with_index do |item, index|
+      item.position = index + 1
+    end
   end
 
   def create
