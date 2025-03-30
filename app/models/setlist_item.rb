21afbb47b02f642888929d3e9b17074f7b9f750f
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-03-30 17:16:36 +0900

    add: setlist_itemsテーブル追加

---
+++ app/models/setlist_item.rb
@@ -0,0 +1,2 @@
+class SetlistItem < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_30_081357) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_30_081505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "setlist_items", force: :cascade do |t|
+    t.string "song_title", null: false
+    t.integer "position", null: false
+    t.integer "setlist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "setlists", force: :cascade do |t|
     t.datetime "created_at", null: false
