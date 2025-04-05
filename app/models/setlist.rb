94f4afdf7ed18f92118f1f1ba7a9a63632ab9489
shimada-ryuga-56a <shimadaryuga@gmail.com>
2025-04-05 15:23:38 +0900

    add: setlist_itemsはデフォルトでpositionで並び替え

--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -1,5 +1,5 @@
 class Setlist < ApplicationRecord
-  has_many :setlist_items, dependent: :destroy, autosave: :true
+  has_many :setlist_items, -> {order(:position)}, dependent: :destroy, autosave: :true
   accepts_nested_attributes_for :setlist_items, allow_destroy: true, reject_if: :all_blank
 
   validates :setlist_items, presence: true
